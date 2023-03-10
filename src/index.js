083a534f31c91f00874f5b55dc5ed4eebde47d09
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-03-10 18:55:41 -0500

    Finished minus styling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 let countEl = document.getElementById("count-el");
-
+let saveEl = document.getElementById("save-el");
 let count = 0;
 
 function increase() {
     ++count
-    countEl.innerText = count
+    countEl.textContent = count
 };
 
 function decrease() {
     --count
-    countEl.innerText = count
+    countEl.textContent = count
 };
 
 function save() {
-    console.log(count);
+    let countStr = count + " / ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
 };
